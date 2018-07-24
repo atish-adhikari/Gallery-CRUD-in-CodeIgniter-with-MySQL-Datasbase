e49f50273e2405baecfa62ea0b1c4f7d79677d2b
Atish Adhikari <atishadhikary@gmail.com>
2018-07-24 23:40:12 +0545

    Revert "Fixed upload and empty array issues"
    This reverts commit e3ce717768750f37375f1a25ed586f38b7fa4969.

--- application/views/templates/album.php
+++ application/views/templates/album.php
@@ -1,37 +1,33 @@
 <div class="container">
     <section class="row">
 
-    <?php 
-    if (isset($albums) && !empty($albums)) {
-        foreach($albums as $key => $value) { ?>
+    <?php foreach($albums as $key => $value) { ?>
 
-            <article class="col-xs-12 col-sm-6 col-md-3">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <a href="<?php echo base_url().'album/'.$value->id?>" title="<?php echo $value->title?>" class="zoom">
-                            <img class="thumb" src="<?php echo base_url().'uploads/gallery/'.$value->featured?>" alt="<?php echo $value->title?>" />
-                            <span class="overlay">
-                                    <i class="glyphicon glyphicon-picture"></i>
-                            </span>
-                        </a>
-                    </div>
-                    <div class="panel-footer">
-                        <h4><a href="<?php echo base_url().'album/'.$value->id?>" title="<?php echo $value->title?>"><?php echo $value->title?></a></h4>
-                        <span style="float: right">
-                            <a class="btn btn-default btn-xs" href="<?php echo base_url().'album/edit/'.$value->id?>" aria-label="Edit">
-                              <i class="glyphicon glyphicon-pencil" aria-hidden="true"></i>
-                            </a>
-                            <a class="btn btn-danger btn-xs" href="<?php echo base_url().'album/deactivate/'.$value->id?>" aria-label="Delete">
-                              <i class="glyphicon glyphicon-trash" aria-hidden="true"></i>
-                            </a>
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="<?php echo base_url().'album/'.$value->id?>" title="<?php echo $value->title?>" class="zoom">
+                        <img class="thumb" src="<?php echo base_url().'uploads/gallery/'.$value->featured?>" alt="<?php echo $value->title?>" />
+                        <span class="overlay">
+                                <i class="glyphicon glyphicon-picture"></i>
                         </span>
-                    </div>
+                    </a>
                 </div>
-            </article>
-        <?php } 
-    } else { ?>   
-        <h2 class="page-title">No Albums Found!</h2>
-    <?php }?>                                         
+                <div class="panel-footer">
+                    <h4><a href="<?php echo base_url().'album/'.$value->id?>" title="<?php echo $value->title?>"><?php echo $value->title?></a></h4>
+                    <span style="float: right">
+                        <a class="btn btn-default btn-xs" href="<?php echo base_url().'album/edit/'.$value->id?>" aria-label="Edit">
+                          <i class="glyphicon glyphicon-pencil" aria-hidden="true"></i>
+                        </a>
+                        <a class="btn btn-danger btn-xs" href="<?php echo base_url().'album/deactivate/'.$value->id?>" aria-label="Delete">
+                          <i class="glyphicon glyphicon-trash" aria-hidden="true"></i>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </article>
+
+<?php } ?>                                        
 
 </section>
 </div>
--- application/views/templates/view_album.php
+++ application/views/templates/view_album.php
@@ -2,20 +2,14 @@
 <link rel="stylesheet" href="<?php echo base_url()?>application/third_party/libs/css/prettyPhoto.css">
 
 <div class="container gallery">
-    <h2 class="page-title"><?php echo isset($record->title) ? $record->title : ''?></h2>
-
-    <?php
-    if (isset($picturelist) && !empty($picturelist)) {
-        foreach($picturelist as $key => $value) { ?>            
-                <ul class="thumbnails">
-                    <li class="span3">
-                            <a class="thumbnail" rel="prettyPhoto[pp_gal]" href="<?php echo base_url().'uploads/gallery/'.$value->image?>" title="<?php echo $value->title?>"><img src="<?php echo base_url().'uploads/gallery/'.$value->image?>" alt="<?php echo $value->title?>" /></a>
-                        </li> <!--end thumb -->
-                </ul><!--end thumbnails -->
-        <?php } 
-    } else { ?>
-        <h4 class="page-title">Album is empty!</h4>
-    <?php  } ?>                   
+    <h2 class="page-title"><?php echo $record->title?></h2>
+    <?php foreach($picturelist as $key => $value) { ?>            
+            <ul class="thumbnails">
+                <li class="span3">
+                        <a class="thumbnail" rel="prettyPhoto[pp_gal]" href="<?php echo base_url().'uploads/gallery/'.$value->image?>" title="<?php echo $value->title?>"><img src="<?php echo base_url().'uploads/gallery/'.$value->image?>" alt="<?php echo $value->title?>" /></a>
+                    </li> <!--end thumb -->
+            </ul><!--end thumbnails -->
+    <?php } ?>                   
 </div> <!-- /container -->
 
 
